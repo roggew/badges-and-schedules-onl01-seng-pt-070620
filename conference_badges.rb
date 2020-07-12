211202d2308d4f12ca438c2490085c73535e8faf
William Rogge <williamroggeiv@gmail.com>
2020-07-12 02:46:47 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+# Write your code here
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  talkers = []
+  attendees.each do |speakers|
+    talkers << "Hello, my name is #{speakers}."
+end
+talkers
+end
+
+def assign_rooms(attendees)
+  location = []
+  attendees.each_with_index do |name, index|
+  location << "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+end
+location
+end
+
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |name|
+    puts "#{name}"
+  end
+  assign_rooms(attendees).each do |room|
+    puts "#{room}"
+  end
+end
